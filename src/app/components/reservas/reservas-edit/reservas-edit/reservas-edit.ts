655eae7713b454ffac106d63aa341d07ee5c0764
JoseAlexander <orozco.jose.7378@eam.edu.co>
2025-11-18 03:01:45 -0500

    ADD FUCTIONS

--- src/app/components/reservas/reservas-edit/reservas-edit/reservas-edit.ts
+++ src/app/components/reservas/reservas-edit/reservas-edit/reservas-edit.ts
@@ -1,17 +1,40 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Reserva } from '../../../../models/reserva';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reservas-edit',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './reservas-edit.html',
   styleUrl: './reservas-edit.scss'
 })
-export class ReservasEdit {
+
+export class ReservasEdit implements OnChanges {
   @Input() isOpen = false
   @Input() reservaToEdit!: Reserva | null
   @Output() onClose = new EventEmitter<void>()
   @Output() onSave = new EventEmitter<Reserva>()
+
+  reserva: Reserva = {
+    id: null,
+    usuario: "",
+    vuelo: "",
+    estado: "",
+    numasiento: ""
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes["reservaToEdit"] && this.reservaToEdit){
+      this.reserva = {...this.reservaToEdit}
+    }
+  }
+
+  closeModal(): void {
+    this.onClose.emit();
+  }
+
+  saveReserva(): void {
+    this.onSave.emit(this.reserva);
+  }
 }
--- src/app/components/reservas/reservas-save/reservas-save/reservas-save.ts
+++ src/app/components/reservas/reservas-save/reservas-save/reservas-save.ts
@@ -1,7 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { Reserva } from '../../../../models/reserva';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Pago } from '../../../../models/pago';
+import { EstadoPago } from '../../../../models/enums/estadoPago';
+import { MetodoPago } from '../../../../models/enums/MetodoPago';
+
+interface ReservaYPago{
+  reserva: Reserva,
+  pago: Pago
+}
 
 @Component({
   selector: 'app-reservas-save',
@@ -9,13 +17,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './reservas-save.html',
   styleUrl: './reservas-save.scss'
 })
-export class ReservasSave {
+
+export class ReservasSave implements OnChanges {
   @Input() isOpen = false
   // 🔹 NUEVO INPUT: Recibe la reserva pre-inicializada desde el componente padre
   @Input() reservaToEdit: Reserva | null = null;
+  @Input() pagoToEdit: Pago | null = null;
   
   @Output() onClose = new EventEmitter<void>()
-  @Output() onSave = new EventEmitter<Reserva>()
+  @Output() onSave = new EventEmitter<ReservaYPago>()
 
   listaVuelos: any[] = []; // Asumo que esta lista se llenará con datos del microservicio
 
@@ -28,13 +38,26 @@ export class ReservasSave {
     numasiento: ""
   }
 
-  ngOnInit(): void {
-    // 🔹 Al inicializar, si se recibe una reserva, la usamos para el formulario
-    if (this.reservaToEdit) {
-      // Clonamos el objeto para evitar modificar el objeto original del padre
+  pago: Pago = {
+    id: null,
+    monto: 0,
+    fecha: new Date(),
+    estado: EstadoPago.PENDIENTE,
+    moneda: "",
+    metodo_pago: MetodoPago.TRANSFERENCIA,
+    reserva: "",
+    usuario: ""
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['reservaToEdit'] && this.reservaToEdit) {
+      console.log(this.reservaToEdit)
       this.reserva = { ...this.reservaToEdit };
     }
-    console.log(this.reserva);
+    if (changes['pagoToEdit'] && this.pagoToEdit) {
+      console.log(this.pagoToEdit)
+      this.pago = { ...this.pagoToEdit };
+    }
   }
 
   closeModal(): void {
@@ -43,6 +66,6 @@ export class ReservasSave {
 
   saveReserva(): void {
     // 🔹 Emitimos la reserva con los datos del formulario
-    this.onSave.emit(this.reserva);
+    this.onSave.emit({reserva: this.reserva, pago: this.pago});
   }
 }
